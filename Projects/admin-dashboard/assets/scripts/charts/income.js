cc5a2e494cbbab96f4c0f093a16e7f27ad59b057
Daniel <dandcalvo@gmail.com>
2022-09-27 16:42:29 -0700

    add content & restructure grid

--- Projects/admin-dashboard/assets/scripts/charts/income.js
+++ Projects/admin-dashboard/assets/scripts/charts/income.js
@@ -1,55 +1,90 @@
 let colors = [
     '#2bd56d',
     '#fa3e3e',
-    ]
-    
-    let labels = [
+]
+
+let labels = [
     'Income',
     'Expense',
-    ];
-    
-    let data = [
-    11889,
-    1558,
-    
-    ];
-    
-    let myChart = document.getElementById('myChart').getContext('2d');
-    
-    let chart = new Chart(myChart, {
-        type: 'doughnut',
-        data: {
-            datasets: [{
-                data: data,
-                backgroundColor: colors
-            }],
-            labels: labels
-        },
-        options: {
-            plugins: {
-                legend: {
-                    display: true,
-                    position: 'bottom',
-    
-                    onHover: handleHover,
-                    onLeave: handleLeave
-                }
-            },
+];
+
+let data = [
+    5100,
+    3587,
+
+];
+
+let myChart = document.getElementById('myChart').getContext('2d');
+
+let chart = new Chart(myChart, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: data,
+            backgroundColor: colors
+        }],
+        labels: labels
+    },
+    options: {
+        plugins: {
+            legend: {
+                display: true,
+                position: 'bottom',
+
+                onHover: handleHover,
+                onLeave: handleLeave
+            }
         }
+    },
+    plugins: [{
+        id: 'text',
+        beforeDraw: function (chart, a, b) {
+            var width = chart.width,
+                height = chart.height,
+                ctx = chart.ctx;
+
+            ctx.restore();
+            var fontSize = (height / 112)-1.6;
+            ctx.font = fontSize + "em Trebuchet MS";
+            ctx.textBaseline = "middle";
+
+            let cashflow = data[0] - data[1];
+            let isNeg = false;
+            if (cashflow < 0) {
+                isNeg = true;
+            }
+
+            cashflow = Math.abs(cashflow).toString();
+
+            if (isNeg) {
+                cashflow = "-$" + cashflow;
+            }
+            else {
+                cashflow = "$" + cashflow;
+            }
+
+            var text = cashflow + '/month',
+                textX = Math.round((width - ctx.measureText(text).width) / 2),
+                textY = (height / 2) - 16;
+
+            ctx.fillText(text, textX, textY);
+            ctx.save();
+        }
+    }]
+});
+
+// Append '4d' to the colors (alpha channel), except for the hovered index
+function handleHover(evt, item, legend) {
+    legend.chart.data.datasets[0].backgroundColor.forEach((color, index, colors) => {
+        colors[index] = index === item.index || color.length === 9 ? color : color + '4D';
     });
-    
-    // Append '4d' to the colors (alpha channel), except for the hovered index
-    function handleHover(evt, item, legend) {
-        legend.chart.data.datasets[0].backgroundColor.forEach((color, index, colors) => {
-            colors[index] = index === item.index || color.length === 9 ? color : color + '4D';
-        });
-        legend.chart.update();
-    }
-    
-    // Removes the alpha channel from background colors
-    function handleLeave(evt, item, legend) {
-        legend.chart.data.datasets[0].backgroundColor.forEach((color, index, colors) => {
-            colors[index] = color.length === 9 ? color.slice(0, -2) : color;
-        });
-        legend.chart.update();
-    }
+    legend.chart.update();
+}
+
+// Removes the alpha channel from background colors
+function handleLeave(evt, item, legend) {
+    legend.chart.data.datasets[0].backgroundColor.forEach((color, index, colors) => {
+        colors[index] = color.length === 9 ? color.slice(0, -2) : color;
+    });
+    legend.chart.update();
+}
