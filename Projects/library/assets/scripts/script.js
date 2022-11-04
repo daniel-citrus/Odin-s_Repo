5e4daaf35a7b82648156383cdc18a808f2f4c1a0
Daniel <dandcalvo@gmail.com>
2022-11-04 15:09:52 -0700

    display books function

--- Projects/library/assets/scripts/script.js
+++ Projects/library/assets/scripts/script.js
@@ -2,30 +2,72 @@ let libraryElement = document.getElementById('library');
 
 let library = [];
 
-function Book(title, author, published, currentPage, pages, read) {
+function Book(title, author, published, currentPage, pages) {
     this.title = title;
     this.author = author;
     this.published = published;
     this.currentPage = currentPage;
     this.pages = pages;
-    this.read = read;
+    this.read = (Boolean(currentPage === pages) ? 'Finished' : 'Not Finished');
 }
 
 function addBookToLibrary(book) {
     library.push(book);
 }
 
-function displayBooks() {
-    for (let book in library) {
-        let node = document.createElement('div');
-        node.textContent = 'book';
-        libraryElement.appendChild(node);
-    }
+function createDiv(divClass) {
+    let div = document.createElement('div');
+    div.classList.add(divClass);
+
+    return div;
 }
 
-let b = new Book('Harry Potter', 'Daniel Calvo', 1999, 210, 500, false);
-addBookToLibrary(b);
-addBookToLibrary(b);
-addBookToLibrary(b);
-addBookToLibrary(b);
+function displayBooks() {
+    let cardBoard = createDiv('card-board');
+    let card, title, author, published, currentPage, pages, read;
+
+    for (let book of library) {
+        card = createDiv('card');
+        title = createDiv('title');
+        author = createDiv('author');
+        published = createDiv('published');
+        currentPage = createDiv('currentPage');
+        pages = createDiv('pages');
+        read = createDiv('read');
+
+        title.textContent = book.title;
+        author.textContent = 'by ' + book.author;
+        published.textContent = 'Published: ' + book.published;
+        currentPage.textContent = book.currentPage;
+        pages.textContent = book.pages;
+        read.textContent = book.read;
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(published);
+        card.appendChild(currentPage);
+        card.appendChild(pages);
+        card.appendChild(read);
+        cardBoard.appendChild(card);
+    }
+
+    libraryElement.appendChild(cardBoard);
+
+}
+
+let books = [
+    ['The Count of Monte Cristo', 'Alexandre Dumas', 1844, 15, 1276],
+    ['Nineteen Eigthy-Four', 'George Orwell', 1949, 328, 328],
+    ['To Kill a Mockingbird', 'Harper Lee', 1960, 100, 281]
+]
+
+function createBook(bookDetails) {
+    let book = new Book(...bookDetails);
+    addBookToLibrary(book);
+}
+
+for (let b of books) {
+    createBook(b);
+}
+
 displayBooks();
