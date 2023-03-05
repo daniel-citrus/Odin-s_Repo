0375d3f28d92d2a059dff1e1c84f5c27f75692f1
Daniel <dandcalvo@gmail.com>
2023-03-04 22:42:23 -0800

    game logic

--- Projects/sim/assets/scripts/script.js
+++ Projects/sim/assets/scripts/script.js
@@ -1,37 +1,53 @@
+const lines = document.querySelectorAll('.line');
+
 // Board brain
 const board = (() => {
-    let clearBoard = () => {
+    const boardMap = new Map();
 
+    let resetBrain = () => {
+        for (let l of lines) {
+            boardMap.set(l.getAttribute('coordinates'), l.getAttribute('marker'));
+        }
     }
+
+    return Object.assign(
+        {},
+        { resetBrain },
+    );
 })();
 
 // Controls the game flow
 const director = (() => {
     currentPlayer = 0; // 0 = player1 | 1 = player2
 
-    let makeMove = () => {
-
+    let makeMove = (coordinates) => {
+        console.log(coordinates);
+        /*
+            update boardBrain
+            update display
+                switch to next player
+                updateMarker
+        */
     }
 
     let restartGame = () => {
         currentPlayer = 0;
-        displayController.restartBoard();
+        displayController.resetBoard();
     }
 
     return Object.assign(
         {},
-        { currentPlayer }
+        { makeMove },
+        { restartGame },
     )
 })();
 
 // Controls the front-end
 const displayController = (() => {
-    const lines = document.querySelectorAll('.line');
-
-    // Initialize eventlisteners for each line and adds a custom attribute called 'marker' that represets player1('0') or player2('1') markings (default value = '')
+    // Initialize eventListeners for each line and add a custom attribute called 'marker' that represets player1('0') or player2('1') markings (default value = '')
     for (let l of lines) {
         l.addEventListener('click', () => {
-
+            director.makeMove(l.getAttribute('coordinates'));
         })
 
         l.setAttribute('marker', '');
@@ -43,7 +59,7 @@ const displayController = (() => {
         }
     }
 
-    let restartBoard = () => {
+    let resetBoard = () => {
         clearBoard();
         updateCurrentPlayer(0);
     }
@@ -54,9 +70,14 @@ const displayController = (() => {
         (player) ? board.classList.add('p2') : board.classList.remove('p2');
     }
 
+    // Updates 'marker' attribute value
+    let updateMarker = (line, player) => {
+        line.setAttribute('marker', player);
+    }
+
     return Object.assign(
         {},
-        { lines },
-        { restartBoard },
+        { resetBoard },
+        { updateMarker },
     )
 })();
