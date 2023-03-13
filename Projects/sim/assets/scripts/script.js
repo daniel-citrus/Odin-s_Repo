f84e80a2ba4d08f07d801852fa587976028b7431
Daniel <dandcalvo@gmail.com>
2023-03-13 14:50:39 -0700

    optimize loser check

--- Projects/sim/assets/scripts/script.js
+++ Projects/sim/assets/scripts/script.js
@@ -23,9 +23,11 @@ const board = (() => {
         (2, 3->6)
         (1, 3)
     */
-    let checkWinner = (line, currentPlayer) => {
-        /* let result = null;
+    /* let checkLoser = (currentPlayer) => {
+        console.clear();
+        let result = null;
         let losingPlayer = null;
+        let losingTriangle = null;
 
         // Checks the entire boardMap for a winning triangle
         for (let [line, player] of boardMap) {
@@ -36,18 +38,25 @@ const board = (() => {
                 break;
             }
         }
-        console.log(`Result: ${result}, Winner: ${losingPlayer}`); */
-    }
+        console.log(`Result: ${result}, Loser: ${losingPlayer}`);
+    } */
 
     /* 
+        **All incoming parameter values must be integers**
         @param level - nth line of the triangle
         @param player - the player that marked [aInitial, bInitial]
         @return - true, if triangle is found and the losing player
     */
     /* let createsTriangle = (level, player, aInitial, bInitial, aCurrent, bCurrent) => {
+        if (!boardMap.has(`${aCurrent},${bCurrent}`)) {
+            return [false, null]
+        }
+
         if (player != boardMap.get(`${aCurrent},${bCurrent}`)) {
             return [false, null];
         }
+
+        console.log(`Player: ${player}, a: ${aCurrent}, b:${bCurrent}`);
 
         if (level == 2) {
             if (boardMap.has(`${aInitial},${bCurrent}`)) {
@@ -61,18 +70,88 @@ const board = (() => {
 
         aCurrent = bCurrent;
         for (let i = aCurrent + 1; i < 7; i++) {
-            if (boardMap.has(`${aCurrent},${i}`)) {
-                return createsTriangle(level + 1, player, aInitial, bInitial, aCurrent, i);
+            let [result, loser] = createsTriangle(level + 1, player, aInitial, bInitial, aCurrent, i);
+
+            if (result) {
+                return [result, loser];
             }
         }
 
         return [false, null];
     } */
 
+    let checkLoser = (line, currentPlayer) => {
+        let [a, b] = line.getAttribute('coordinates').split(',');
+        let result = createsTriangle(1, currentPlayer, +a, +b, +a, +b);
+
+        if (result) {
+            let winner = (currentPlayer) ? "Blue" : "Red";
+
+            console.log(`${winner} wins!`);
+        }
+
+    }
+
+    let createsTriangle = (level, player, aI, bI, aC, bC) => {
+        if (!boardMap.has(`${aC},${bC}`)) {
+            return false;
+        }
+
+        if (player != boardMap.get(`${aC},${bC}`)) {
+            return false;
+        }
+
+        if (level == 2) {
+            let a, b;
+            a = aC;
+            b = aI;
+
+            if (a > b) {
+                [a, b] = swap(a, b);
+            }
+
+            if (boardMap.has(`${a},${b}`)) {
+                if (player == boardMap.get(`${a},${b}`)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        for (let i = 1; i < 7; i++) {
+            let a, b;
+
+            if (a == i || b == i) {
+                continue;
+            }
+
+            a = bC;
+            b = i;
+
+            if (a > b) {
+                [a, b] = swap(a, b);
+            }
+
+            if (boardMap.has(`${a},${b}`)) {
+                if (player == boardMap.get(`${a},${b}`)) {
+                    if (createsTriangle(level + 1, player, aI, bI, a, b)) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    /* Swaps two variables and returns them */
+    let swap = (a, b) => {
+        return [b, a];
+    }
+
     return Object.assign(
         {},
         {
-            checkWinner,
+            checkLoser,
             resetBrain,
             updateBrain,
         },
@@ -84,7 +163,6 @@ const director = (() => {
     currentPlayer = 0; // 0 = player1 | 1 = player2
 
     let makeMove = (line) => {
-        console.clear();
         if (line.getAttribute('marker') !== '') {
             console.log(`Invalid move`);
             return;
@@ -92,7 +170,8 @@ const director = (() => {
 
         board.updateBrain(line, currentPlayer);
         displayController.updateMarker(line, currentPlayer);
-        board.checkWinner(line, currentPlayer);
+        /* board.checkLoser(currentPlayer); */
+        board.checkLoser(line, currentPlayer);
         currentPlayer = 1 - currentPlayer;
         displayController.updateCurrentPlayer(currentPlayer);
     }
