df73100d8affbe05525a40780dd0cc01a569ede1
Daniel <dandcalvo@gmail.com>
2023-03-21 16:03:31 -0700

    track losing triangle

--- Projects/sim/assets/scripts/script.js
+++ Projects/sim/assets/scripts/script.js
@@ -1,44 +1,46 @@
 const lines = document.querySelectorAll('.line');
 
-// Board brain
+/* Board brain */
 const board = (() => {
     const boardMap = new Map();
-    let losingTriangle = null;
+    let losingTriangle = [];
 
     let resetBrain = () => {
         boardMap.clear();
-        /*
-        for (let l of lines) {
-            boardMap.set(l.getAttribute('coordinates'), '');
-        }
-        */
     }
 
     let updateBrain = (line, marker) => {
         boardMap.set(line.getAttribute('coordinates'), marker);
     }
 
-    /*
-        Checks the entire boardMap for a winning triangle
-    */
+    /* Checks the entire boardMap for a winning triangle */
     let checkLoser = (currentPlayer) => {
-        console.clear();
         let result = null;
-        let losingPlayer = null;
 
         for (let [line, player] of boardMap) {
+            losingTriangle = [];
+
             if (player != currentPlayer) {
                 continue;
             }
 
             let [a, b] = line.split(',');
-            [result, losingPlayer] = createsTriangle(1, currentPlayer, +a, +b, +a, +b);
+            losingTriangle.push([+a, +b]);
+            result = createsTriangle(1, currentPlayer, +a, +b, +a, +b);
 
             if (result) {
+                console.log(`Winner: ${1 - currentPlayer}`);
+                for (let l of losingTriangle) {
+                    console.log(l);
+                }
                 break;
             }
         }
-        console.log(`Result: ${result}, Loser: ${losingPlayer}`);
+
+        return {
+            result,
+            losingTriangle
+        }
     }
 
     /* 
@@ -47,40 +49,46 @@ const board = (() => {
         **All incoming parameter values must be integers**
         @param level - nth line of the triangle
         @param player - the player that marked [aInitial, bInitial]
-        @return - true, if triangle is found and the losing player
+        @param aInitial - starting point of the line marked by player
+        @param bInitial - end point of the line marked by player
+        @param a - current line's starting point
+        @param b - current line's end point
+        @return (boolean, value) - true, if triangle is found and the losing player
     */
     let createsTriangle = (level, player, aInitial, bInitial, a, b) => {
-        console.log(`a: ${a}, b: ${b}`);
         if (!boardMap.has(`${a},${b}`)) {
-            return [false, null]
+            return false;
         }
-        
+
         if (player != boardMap.get(`${a},${b}`)) {
-            return [false, null];
+            return false;
         }
-        
+
         // Condition for a complete triangle
         if (level == 2) {
-            if (boardMap.has(`${aInitial},${b}`)) {
-                console.log(`a: ${aInitial}, b: ${b}`);
-                console.log(boardMap);
-                if (player == boardMap.get(`${aInitial},${b}`)) {
-                    return [true, player];
-                }
+            if (!boardMap.has(`${aInitial},${b}`)) {
+                return false;
             }
-            return [false, null];
+
+            if (player == boardMap.get(`${aInitial},${b}`)) {
+                losingTriangle.push([aInitial, b]);
+                return true;
+            }
+
+            return false;
         }
 
         a = b;
         for (let i = a + 1; i < 7; i++) {
-            let [result, loser] = createsTriangle(level + 1, player, aInitial, bInitial, a, i);
+            let result = createsTriangle(level + 1, player, aInitial, bInitial, a, i);
 
             if (result) {
-                return [result, loser];
+                losingTriangle.push([a, i]);
+                return result;
             }
         }
 
-        return [false, null];
+        return false;
     }
 
     return Object.assign(
@@ -97,7 +105,13 @@ const board = (() => {
 const director = (() => {
     currentPlayer = 0; // 0 = player1 | 1 = player2
 
+    let endGame = (winner, losingTriangle) => {
+        // 
+    }
+
     let makeMove = (line) => {
+        console.clear();
+
         if (line.getAttribute('marker') !== '') {
             console.log(`Invalid move`);
             return;
@@ -105,7 +119,9 @@ const director = (() => {
 
         board.updateBrain(line, currentPlayer);
         displayController.updateMarker(line, currentPlayer);
+
         board.checkLoser(currentPlayer);
+
         currentPlayer = 1 - currentPlayer;
         displayController.updateCurrentPlayer(currentPlayer);
     }
@@ -128,14 +144,12 @@ const director = (() => {
 // Controls the front-end
 const displayController = (() => {
     let z_index = 1; // Most recently marked line will appear above all other lines
-    
-    // Initialize eventListeners for each line and add a custom attribute called 'marker' that represets player1('0') or player2('1') markings (default value = '')
+
+    // Initialize eventListeners for each line
     for (let l of lines) {
         l.addEventListener('click', () => {
             director.makeMove(l);
         })
-
-        l.setAttribute('marker', '');
     }
 
     let clearBoard = () => {
@@ -152,7 +166,6 @@ const displayController = (() => {
 
     let updateCurrentPlayer = (player) => {
         let board = document.querySelector('.board');
-
         (player) ? board.classList.add('p2') : board.classList.remove('p2');
     }
 
