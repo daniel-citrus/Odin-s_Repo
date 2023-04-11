93e02fdfda1ad3ddb424bc88c658189ec169414d
Daniel <dandcalvo@gmail.com>
2023-04-11 16:05:01 -0700

    cleaning

--- Exercises/study-session-tasks/webpack-demo/webpack.config.js
+++ Exercises/study-session-tasks/webpack-demo/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: ['/src/index.js', '/src/other.js'],
     output: {
-        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
     },
 };
