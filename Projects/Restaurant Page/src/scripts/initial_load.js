5d988381d3f8575b9bf7aa587d9e8f90c41e174f
Daniel <dandcalvo@gmail.com>
2023-05-18 16:07:15 -0700

    add bg

--- Projects/Restaurant Page/src/scripts/initial_load.js
+++ Projects/Restaurant Page/src/scripts/initial_load.js
@@ -3,12 +3,13 @@ import about from './pages/about';
 import contact from './pages/contact';
 import menu from './pages/menu';
 
+let content = document.getElementById('content');
 
 /**
  * Generates navigation buttons and then calls the homepage module
  */
 (() => {
-    let content = document.getElementById('content');
+    buildHeader();
 
     let pages = {
         'About': about,
@@ -19,11 +20,28 @@ import menu from './pages/menu';
     Object.keys(pages).forEach((page) => {
         content.appendChild(createButton(page, pages[page]));
     });
+
+    buildFooter();
 })();
 
 /**
  * Builds the header, footer, and background of the website
- */
+*/
+function buildHeader() {
+    let header = document.createElement('div');
+
+    header.classList.add('header');
+    header.textContent = 'Header';
+    content.appendChild(header);
+}
+
+function buildFooter() {
+    let footer = document.createElement('div');
+
+    footer.classList.add('footer');
+    footer.textContent = 'Footer';
+    content.appendChild(footer);
+}
 
 /**
  * Returns a button element with an eventlistener that executes the action
@@ -35,10 +53,10 @@ import menu from './pages/menu';
 function createButton(name, action) {
     let button = document.createElement('button');
 
+    button.classList.add('nav');
     button.textContent = name;
     button.title = name;
     button.type = 'button';
-    button.classList.add('nav');
 
     button.addEventListener('click', () => {
         action();
@@ -46,7 +64,3 @@ function createButton(name, action) {
 
     return button;
 }
-
-// About
-// Menu
-// Location
