8b1b6c57955598db73b43073bd0909c6a178c105
Daniel <dcalvo@leomail.tamuc.edu>
2023-12-06 16:27:15 -0800

    eslint

--- Projects/sign-up-form2/.eslintrc.js
+++ Projects/sign-up-form2/.eslintrc.js
@@ -1,25 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['airbnb-base', 'prettier'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['./src/**/*.js'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    quotes: [2, 'single'],
-  },
+    extends: ['airbnb-base', 'prettier'],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['./src/**/*.js'],
+        },
+    ],
+    rules: {
+        quotes: [2, 'single'],
+        indent: [2, 4],
+    },
+    parserOptions: {
+        sourceType: 'module',
+    },
 };
