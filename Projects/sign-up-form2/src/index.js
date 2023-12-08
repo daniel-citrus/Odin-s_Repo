a8425d993aaed404d5e0dfd887d7b50cd713ef1c
Daniel <dcalvo@leomail.tamuc.edu>
2023-12-07 17:04:15 -0800

    validators

--- Projects/sign-up-form2/src/index.js
+++ Projects/sign-up-form2/src/index.js
@@ -1,7 +1,12 @@
 import * as zipTools from './zipRegex';
 
 const submitButton = document.querySelector('form button[type="button"]');
-const countrySelect = document.getElementById('country');
+const emailInput = document.getElementById('email');
+const emailValidate = document.querySelector('.email .validate');
+const countryInput = document.getElementById('country');
+const postalInput = document.getElementById('postal');
+const passInput = document.getElementById('password');
+const confirmPassInput = document.getElementById('confirmPassword');
 
 function buildCountryOptions() {
     zipTools.getCountryCodes().forEach((countryCode) => {
@@ -19,15 +24,42 @@ function buildCountryOptions() {
             option.selected = true;
         }
 
-        countrySelect.appendChild(option);
+        countryInput.appendChild(option);
     });
 }
+
+function validateEmail() {
+    const emailRegex =
+        /^([a-zA-Z0-9]{1,}@[a-zA-Z0-9]{1,}).[a-zA-Z0-9]{1,}(.[a-zA-Z0-9]{1,}(.[a-zA-Z0-9]{1,})?)?$/gm;
+
+    if (emailRegex.test(emailInput.value)) {
+        emailValidate.classList.remove('flag');
+        emailValidate.textContent = '';
+        return true;
+    }
+
+    emailValidate.classList.add('flag');
+    emailValidate.textContent =
+        'Please enter a valid email (e.g. bob@gmail.com)';
+
+    return false;
+}
+
+function validateInputs() {}
 
 (() => {
     buildCountryOptions();
 
+    const inputEventTypes = ['focusout', 'keyup', 'input'];
+
+    inputEventTypes.forEach((inputType) => {
+        emailInput.addEventListener(inputType, () => {
+            validateEmail();
+        });
+    });
+
     submitButton.addEventListener('click', (e) => {
         e.preventDefault();
-        //validateInputs();
+        validateInputs();
     });
 })();
